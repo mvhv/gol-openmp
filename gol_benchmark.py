e9dd3aa566f15da578543d1114022832cd5a9a23
Jesse Wyatt <wyattjrw@gmail.com>
2018-09-14 00:44:33 +0800

    basic benchmark graphing

--- gol_benchmark.py
+++ gol_benchmark.py
@@ -1,9 +1,11 @@
 import subprocess as sp
-from matplotlib import pyplot
+import numpy as np
+from matplotlib import pyplot as plt
 
 test_sizes = ["128", "256", "512", "1024", "2048"]
 tests = ["serial", "parallel"]
 
+"""
 #compile
 sp.run(["gcc", "gol_serial.c", "-o", "gol_serial"])
 sp.run(["gcc", "-fopenmp", "gol_parallel.c", "-o", "gol_parallel"])
@@ -24,3 +26,30 @@ for t, test in enumerate(tests):
         user_time[t][s] /= 10
 print(real_time)
 print(user_time)
+"""
+
+real_time = [[0.01, 0.05, 0.211, 0.892, 3.438], [0.012, 0.029, 0.067, 0.243, 0.973]]
+user_time = [[0.01, 0.05, 0.211, 0.891, 3.436], [0.1, 0.223, 0.508, 1.717, 6.344]]
+
+ind = np.arange(len(test_sizes))
+width = 0.3
+
+p1 = plt.bar(ind-width/2, real_time[0], width=width)
+p2 = plt.bar(ind+width/2, real_time[1], width=width)
+plt.title("Elapsed Real Time")
+plt.ylabel("Time (s)")
+plt.xlabel("Grid Dimension (cells)")
+plt.xticks(ind, test_sizes)
+plt.legend([p1[0], p2[0]], tests)
+plt.savefig("realtime.png", bbox_inches="tight")
+plt.cla()
+
+p1 = plt.bar(ind-width/2, user_time[0], width=width)
+p2 = plt.bar(ind+width/2, user_time[1], width=width)
+plt.title("Equivalent User-mode Time")
+plt.ylabel("Time (s)")
+plt.xlabel("Grid Dimension (cells)")
+plt.xticks(ind, test_sizes)
+plt.legend([p1[0], p2[0]], tests)
+plt.savefig("usertime.png", bbox_inches="tight")
+plt.cla()
