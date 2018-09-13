ccb29c0aaec261e2c201cd5d5621f067eeb345cd
Jesse Wyatt <wyattjrw@gmail.com>
2018-09-14 01:02:15 +0800

    refactored harnesses and switched to agg renderer

--- gol_benchmark.py
+++ gol_benchmark.py
@@ -1,11 +1,12 @@
 import subprocess as sp
 import numpy as np
+import matplotlib
+matplotlib.use("Agg") //backend renderer
 from matplotlib import pyplot as plt
 
 test_sizes = ["128", "256", "512", "1024", "2048"]
 tests = ["serial", "parallel"]
 
-"""
 #compile
 sp.run(["gcc", "gol_serial.c", "-o", "gol_serial"])
 sp.run(["gcc", "-fopenmp", "gol_parallel.c", "-o", "gol_parallel"])
@@ -24,16 +25,11 @@ for t, test in enumerate(tests):
             user_time[t][s] += float(output[1])
         real_time[t][s] /= 10
         user_time[t][s] /= 10
-print(real_time)
-print(user_time)
-"""
-
-real_time = [[0.01, 0.05, 0.211, 0.892, 3.438], [0.012, 0.029, 0.067, 0.243, 0.973]]
-user_time = [[0.01, 0.05, 0.211, 0.891, 3.436], [0.1, 0.223, 0.508, 1.717, 6.344]]
 
 ind = np.arange(len(test_sizes))
 width = 0.3
 
+#plot realtime
 p1 = plt.bar(ind-width/2, real_time[0], width=width)
 p2 = plt.bar(ind+width/2, real_time[1], width=width)
 plt.title("Elapsed Real Time")
@@ -42,8 +38,9 @@ plt.xlabel("Grid Dimension (cells)")
 plt.xticks(ind, test_sizes)
 plt.legend([p1[0], p2[0]], tests)
 plt.savefig("realtime.png", bbox_inches="tight")
-plt.cla()
+plt.clf()
 
+#plot usertime
 p1 = plt.bar(ind-width/2, user_time[0], width=width)
 p2 = plt.bar(ind+width/2, user_time[1], width=width)
 plt.title("Equivalent User-mode Time")
@@ -52,4 +49,4 @@ plt.xlabel("Grid Dimension (cells)")
 plt.xticks(ind, test_sizes)
 plt.legend([p1[0], p2[0]], tests)
 plt.savefig("usertime.png", bbox_inches="tight")
-plt.cla()
+plt.clf()
--- gol_snapshot.py
+++ gol_snapshot.py
@@ -1,37 +1,30 @@
-import subprocess
-import sys
-from matplotlib import pyplot
-import time
+import subprocess as sp
+import matplotlib
+matplotlib.use("Agg") //backend renderer
+from matplotlib import pyplot as plt
 
 test_sizes = ["128", "256", "512", "1024", "2048"]
 tests = ["serial", "parallel"]
 
 #compile
-subprocess.run(["gcc", "gol_serial.c", "-o", "gol_serial"])
-subprocess.run(["gcc", "-fopenmp", "gol_parallel.c", "-o", "gol_parallel"])
+sp.run(["gcc", "gol_serial.c", "-o", "gol_serial"])
+sp.run(["gcc", "-fopenmp", "gol_parallel.c", "-o", "gol_parallel"])
 
 #run and capture snapshots
 for test in tests:
     for size in test_sizes:
         with open(test+size+".out", "w") as file:
-            subprocess.run(["./gol_"+test, size], stdout=file)
+            sp.run(["./gol_"+test, size], stdout=file)
 
 #plot snapshots
 for test in tests:
     for size in test_sizes:
         with open(test+size+".out", "r") as file:
-            #read file
-            #remove final newline
-            #split on empty lines
-            #split on lines
-            #remove newlines
-            #split to chars
-            #chars to floats
+            #parse data to 3D float array
             data = [[[float(z) for z in list(y)] for y in x.split()] for x in file.read().strip().split("\n\n")]
             for step, state in enumerate(data):
-                plot = pyplot.imshow(state)
-                plot.set_cmap("Greys")
-                pyplot.title("GoL-"+size+"-"+test+" state at step "+str(step*10))
-                pyplot.savefig(test+size+"_"+str(step*10)+".png", bbox_inches="tight")
-                pyplot.close()
+                p1 = plt.imshow(state)
+                p1.set_cmap("Greys")
+                plt.title("GoL-"+size+"-"+test+" state at step "+str(step*10))
+                plt.savefig(test+size+"_"+str(step*10)+".png", bbox_inches="tight")
 
